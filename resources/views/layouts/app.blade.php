85527f291e5cacdfd0f1957ab78fa3d80cb42ebc
chiuwa-company <chiuwa94@gmail.com>
2021-05-04 11:59:21 +0800

    fix order page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -205,17 +205,17 @@ $carousel  = HomeController::getCarousel();
         <div class="modal_bubble" style="display: none">{{$cart_number}}</div>
       @endif
          <div class="member_button d-flex justify-content-center">
-          <a class="btn member-btn d-flex justify-content-center" href="shopping_cart"><p> <i class="fa fa-shopping-cart member-icon mr-3" aria-hidden="true"></i> MY SHOPPING CART</p></a>
+          <a class="btn member-btn d-flex justify-content-center" href="/shopping_cart"><p> <i class="fa fa-shopping-cart member-icon mr-3" aria-hidden="true"></i> MY SHOPPING CART</p></a>
         </div>
         @if($order_number!=0)
          <div class="modal_bubble_2">{{$order_number}}</div>
                @endif
         <div class="member_button d-flex justify-content-center">
-          <a class="btn member-btn d-flex justify-content-center" href="user_profile"><p> <i class="fa fa-user member-icon mr-3" aria-hidden="true"></i> PROFILE SETTINGS</p></a>
+          <a class="btn member-btn d-flex justify-content-center" href="/user_profile"><p> <i class="fa fa-user member-icon mr-3" aria-hidden="true"></i> PROFILE SETTINGS</p></a>
         </div>
      
         <div class="login_submit_button">
-          <a class="btn logout-btn text-center" href="logout">@lang('frontend.logout')</a>
+          <a class="btn logout-btn text-center" href="/logout">@lang('frontend.logout')</a>
         </div>
       </div>
 
